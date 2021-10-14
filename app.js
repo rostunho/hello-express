59342cf01afc8384ce77cc8e6d44ec18028d0e68
Rostyk Podolets <ros.podolets@gmail.com>
2021-10-14 21:18:41 +0300

    point 01
    End of Tutorial

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const exHbs = require('express-handlebars');
+const products = require('./products.json');
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'hbs');
+app.engine(
+  'hbs',
+  exHbs({
+    extname: 'hbs',
+  }),
+);
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { cssFileName: 'about' });
+});
+
+app.get('/products', (req, res) => {
+  res.render('products', { products, cssFileName: 'products' });
+});
+
+app.get('/product/:productId', (req, res) => {
+  console.log(req.params);
+
+  const product = products.find(p => p.id === req.params.productId);
+
+  res.render('product', { product });
+});
+
+app.listen(4444, () => {
+  console.log(`Aplication Server працює на порті ${4444}`);
+});
